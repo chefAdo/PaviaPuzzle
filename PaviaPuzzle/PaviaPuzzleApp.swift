307a4b7797e8c29537da4e720fa90b0e8e4a34ce
Adahan <adahan@olict.com>
2024-12-16 15:26:50 +0100

    Initial Commit

---
+++ PaviaPuzzle/PaviaPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaviaPuzzleApp.swift
+//  PaviaPuzzle
+//
+//  Created by Adahan on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PaviaPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
